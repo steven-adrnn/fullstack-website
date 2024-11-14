b7f96d8dfa43b1bdb65ccd22ba2b4c79d2631029
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-14 22:31:00 +0700

    .

--- frontend/source/kuis.js
+++ frontend/source/kuis.js
@@ -188,38 +188,3 @@ function startTimer() {
         }
     }, 1000);
 }
-
-
-
-// Mengambil data dari API
-fetch('https://eb00-103-129-95-233.ngrok-free.app/api/profiles/') // Ganti dengan URL ngrok Anda
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Tampilkan data di konsol
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-// Mengirim data ke API
-const newUserProfile = {
-    username: 'new_user',
-    score: 100,
-    // Tambahkan field lain sesuai model
-};
-
-fetch('https://eb00-103-129-95-233.ngrok-free.app/api/profiles/', { // Ganti dengan URL ngrok Anda
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        // Sertakan token CSRF jika diperlukan
-    },
-    body: JSON.stringify(newUserProfile)
-})
-.then(response => response.json())
-.then(data => {
-    console.log('Success:', data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
